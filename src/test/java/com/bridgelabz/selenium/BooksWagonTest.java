ad3b18c97321db2b1f0050d76c372d5f78cfb57f
TuChawat <76853681+TuChawat@users.noreply.github.com>
2021-06-30 13:48:05 +0530

    Update BooksWagonTest.java

--- src/test/java/com/bridgelabz/selenium/BooksWagonTest.java
+++ src/test/java/com/bridgelabz/selenium/BooksWagonTest.java
@@ -28,7 +28,42 @@ public class BooksWagonTest extends Base {
     public Shipping_Address Address;
     public Payment_Method Payment;
 
-    = new Place_Order(driver);
+    @Test(dataProvider="testdata")
+    public void Login_Bookswagon(String username, String password) throws IOException {
+        login = new Login_Bookswagon(driver);
+        login.loggedin(username,password);
+        String accountxpath = Library.getProperty(IConstant.CONFIG_PATH, "accountxpath");
+        String Account = driver.findElement(By.xpath(accountxpath)).getText();
+        Assert.assertEquals("My Account", Account);
+        if (Account.equals("My Account")){
+            test.log(LogStatus.PASS, "Navigated to My account");
+            test.log(LogStatus.PASS, test.addScreenCapture(capture(driver))+ "Login Test Passed");
+        }else {
+            test.log(LogStatus.FAIL, "Test Failed");
+            test.log(LogStatus.PASS, test.addScreenCapture(capture(driver))+ "Login Test Failed");
+        }
+    }
+
+    @Test
+    public void Search_Book() throws InterruptedException, IOException {
+        Search = new Search_Book(driver);
+        Search.SearchItem();
+        Thread.sleep(2000);
+        String bookxpath = Library.getProperty(IConstant.CONFIG_PATH, "bookxpath");
+        String Book = driver.findElement(By.xpath(bookxpath)).getText();
+        Assert.assertEquals("John Green", Book);
+        if (Book.equals("John Green")){
+            test.log(LogStatus.PASS, "Navigated to Book");
+            test.log(LogStatus.PASS, test.addScreenCapture(capture(driver))+ "Login Test Passed");
+        }else {
+            test.log(LogStatus.FAIL, "Test Failed");
+            test.log(LogStatus.PASS, test.addScreenCapture(capture(driver))+ "Login Test Failed");
+        }
+    }
+
+    @Test
+    public void placeOrder() throws IOException, InterruptedException {
+        Order = new Place_Order(driver);
         Order.OrderPlace();
         Order.cart();
         String addressxpath = Library.getProperty(IConstant.CONFIG_PATH, "addressxpath");
@@ -75,17 +110,17 @@ public class BooksWagonTest extends Base {
         }
     }
 
-//    @DataProvider(name="testdata")
-//    public Object[][] testDataExample(){
-//        ReadExcelFile configuration = new ReadExcelFile("C:\\Users\\Admin\\BooksWagon\\Bookswagon.xlsx");
-//        int rows = configuration.getRowCount(0);
-//        Object[][]signin_credentials = new Object[rows][2];
-//
-//        for(int i=0;i<rows;i++)
-//        {
-//            signin_credentials[i][0] = configuration.getData(0, i, 0);
-//            signin_credentials[i][1] = configuration.getData(0, i, 1);
-//        }
-//        return signin_credentials;
-//    }
+    @DataProvider(name="testdata")
+    public Object[][] testDataExample(){
+        ReadExcelFile configuration = new ReadExcelFile("C:\\Users\\Admin\\BooksWagon\\Bookswagon.xlsx");
+        int rows = configuration.getRowCount(0);
+        Object[][]signin_credentials = new Object[rows][2];
+
+        for(int i=0;i<rows;i++)
+        {
+            signin_credentials[i][0] = configuration.getData(0, i, 0);
+            signin_credentials[i][1] = configuration.getData(0, i, 1);
+        }
+        return signin_credentials;
+    }
 }
